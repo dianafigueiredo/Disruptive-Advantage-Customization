97dc76e30debed2d5df4617080b14bb2ff01c0bd
Diana Figueiredo <diana.figueiredo@findmore.eu>
2021-01-21 10:06:03 +0000

    First Commit

---
+++ Disruptive Advantage Customization/JobPostCreate.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace Disruptive_Advantage_Customization
+{
+    public class PostCreate_sync : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            try
+            {
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    Entity targetEntity = (Entity)context.InputParameters["Target"];
+
+
+                    var vesselInfo = targetEntity.GetAttributeValue<EntityReference>("dia_vessel");
+
+
+
+                    var quantity = targetEntity.GetAttributeValue<decimal?>("dia_quantity") == null ? 0 : targetEntity.GetAttributeValue<decimal>("dia_quantity");
+
+                    var type = targetEntity.GetAttributeValue<OptionSetValue>("dia_type");
+
+
+                    // var referencetype = targetEntity.GetAttributeValue<OptionSetValue>("dia_referencetype");
+
+
+                    // var location = targetEntity.GetAttributeValue<EntityReference>("dia_location");
+
+
+                    var batch = targetEntity.GetAttributeValue<EntityReference>("dia_batch");
+
+
+
+                    // var stage = targetEntity.GetAttributeValue<EntityReference>("dia_stage");
+
+
+                    //   var reference = targetEntity.GetAttributeValue<string>("dia_reference");
+
+
+
+                    #region Update Vessel. Job type Intake
+                    if (type.Value == 914440002 && vesselInfo != null)
+                    {
+
+                        var vesselCapacity = service.Retrieve(vesselInfo.LogicalName, vesselInfo.Id, new ColumnSet("dia_capacity"));
+
+                        if (vesselCapacity != null && vesselCapacity.Contains("dia_capacity"))
+                        {
+                            var remainingCapacity = vesselCapacity.GetAttributeValue<decimal>("dia_capacity") - quantity;
+                            var vesselUpdate = new Entity(vesselInfo.LogicalName);
+                            vesselUpdate.Id = vesselInfo.Id;
+                            vesselUpdate.Attributes["dia_occupation"] = quantity;
+                            vesselUpdate.Attributes["dia_remainingcapacity"] = remainingCapacity;
+
+                            service.Update(vesselUpdate);
+                        }
+                    }
+                    #endregion
+                    #region create transaction 
+
+
+                    {
+
+                        var createTransaction = new Entity("dia_vesselstocktransactions");
+
+                        createTransaction.Attributes["dia_quantity"] = quantity;
+                        //   createTransaction.Attributes["dia_referencetype"] = referencetype;
+
+                        // createTransaction.Attributes["dia_location"] = location;
+
+                        createTransaction.Attributes["dia_batch"] = batch;
+                        //createTransaction.Attributes["dia_stage"] = stage;
+
+                        //  createTransaction.Attributes["dia_reference"] = reference;
+
+
+                        service.Create(createTransaction);
+                    }
+
+
+                    #endregion
+
+                    #region Assign composition from batch to vessel
+                    
+                    var batchComposition = batch != null ? service.Retrieve(batch.LogicalName, batch.Id, new ColumnSet("dia_composition")) : null; 
+                    if (type.Value == 914440002 && batchComposition != null)
+                    {
+                        if(vesselInfo != null)
+                        {
+                            var name= targetEntity.GetAttributeValue<string>("dia_name");
+                            var occupation = targetEntity.GetAttributeValue<decimal>("dia_occupation");
+                            var location = targetEntity.GetAttributeValue<EntityReference>("dia_location");
+                            var batchVessel= targetEntity.GetAttributeValue<EntityReference>("dia_batch");
+                            var compositionVessel = targetEntity.GetAttributeValue<EntityReference>("dia_composition");
+                            var capacityVessel = targetEntity.GetAttributeValue<EntityReference>("dia_capacity");
+
+                            var createVessel = new Entity("dia_vessel");
+
+                            createVessel.Attributes["dia_name"] = name;
+                            createVessel.Attributes["dia_occupation"] = occupation;
+                            createVessel.Attributes["dia_location"] = location;
+                            createVessel.Attributes["dia_batch"] = batchVessel;
+                            createVessel.Attributes["dia_composition"] = compositionVessel;
+                            createVessel.Attributes["dia_capacity"] = capacityVessel;
+
+
+                            service.Create(createVessel);
+
+                        }
+                    }
+
+                     
+                    #endregion
+
+                }
+              
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+    }
+}
---
+++ Disruptive Advantage Customization/JobPreCreate.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace Disruptive_Advantage_Customization
+{
+    public class JobPreCreate : IPlugin
+    {
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            try
+            {
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    Entity targetEntity = (Entity)context.InputParameters["Target"];
+                 
+                    var status = targetEntity.GetAttributeValue<OptionSetValue>("statuscode");
+                 
+                    var batch = targetEntity.GetAttributeValue<EntityReference>("dia_batch");
+                    
+                    var batchstatus = service.Retrieve(batch.LogicalName, batch.Id, new ColumnSet("statuscode"));
+                  
+                    if (batchstatus != null && batchstatus.GetAttributeValue<OptionSetValue>("statuscode").Value == 914440000)
+                    {
+
+                        throw new InvalidPluginExecutionException("Can't create a job because the batch isn't finished yet");
+
+                    }
+
+                    #region vessel empty or full  
+
+                    var type = targetEntity.GetAttributeValue<OptionSetValue>("dia_type");
+                    /*var occupation = targetEntity.GetAttributeValue<decimal>("dia_occupation");
+                    var capacity = targetEntity.GetAttributeValue<decimal>("dia_capacity");*/
+                    var vessel = targetEntity.GetAttributeValue<EntityReference>("dia_vessel");
+
+              /*
+                    var vesselInfo = vessel != null ? service.Retrieve(vessel.LogicalName, vessel.Id, new ColumnSet("dia_occupation")) : null;
+                 
+                    var occupation = Decimal.ToInt32(vesselInfo.GetAttributeValue<decimal>("dia_occupation"));
+                    if (type.Value == 914440002 && occupation != 0)
+                    {
+                       
+                        throw new InvalidPluginExecutionException("Can't create a job because the destination vessel isn't empty ");
+                       
+                    }*/
+                    #endregion
+
+
+                    #region Date Verification (Schedule Start and Schedule End)
+
+                    if(vessel != null)
+                    {
+                     
+                        var scheduleStart = targetEntity.GetAttributeValue<DateTime>("dia_schelduledstart");
+                        var scheduleEnd = targetEntity.GetAttributeValue<DateTime>("dia_schelduledfinish");
+                        var queryJob = new QueryExpression("dia_job");
+                        queryJob.ColumnSet.AddColumns("dia_schelduledstart", "dia_schelduledfinish");
+                        queryJob.Criteria.AddCondition("dia_vessel", ConditionOperator.Equal, vessel.Id);
+
+                        EntityCollection resultsqueryJob = service.RetrieveMultiple(queryJob);
+                    
+                        foreach (var job in resultsqueryJob.Entities)
+                        {
+                            
+                            if (scheduleStart > job.GetAttributeValue<DateTime>("dia_schelduledstart") && scheduleStart < job.GetAttributeValue<DateTime>("dia_schelduledfinish"))
+                            {
+                                throw new InvalidPluginExecutionException("Can't create a job because the vessel is not empty in that schedule");
+                            }
+                            if (scheduleEnd > job.GetAttributeValue<DateTime>("dia_schelduledstart") && scheduleEnd < job.GetAttributeValue<DateTime>("dia_schelduledfinish"))
+                            {
+                                throw new InvalidPluginExecutionException("Can't create a job because the vessel is not empty in that schedule");
+                            }
+                        }
+                    }
+
+                    #endregion
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+    }
+}
