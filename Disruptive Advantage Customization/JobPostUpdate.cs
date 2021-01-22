cc993dad8bdb4ab9dc41f14fe4e394a19cd192be
Diana Figueiredo <diana.figueiredo@findmore.eu>
2021-01-22 15:59:49 +0000

    JobUpdate: Job type transfer, source vessel update after completing job

--- Disruptive Advantage Customization/JobPostUpdate.cs
+++ Disruptive Advantage Customization/JobPostUpdate.cs
@@ -23,9 +23,7 @@ namespace Disruptive_Advantage_Customization
                     if (targetEntity.Contains("statuscode") && targetEntity.GetAttributeValue<OptionSetValue>("statuscode").Value == 914440000)
                     {
                         var statuscode = targetEntity.GetAttributeValue<OptionSetValue>("statuscode");
-                        var vesselInfo = targetEntity.GetAttributeValue<EntityReference>("dia_vessel");
                         var quantity = targetEntity.GetAttributeValue<decimal?>("dia_quantity") == null ? 0 : targetEntity.GetAttributeValue<decimal>("dia_quantity");
-                        var vesselCapacity = service.Retrieve(vesselInfo.LogicalName, vesselInfo.Id, new ColumnSet("dia_capacity"));
                         /*if (vesselCapacity != null && vesselCapacity.Contains("dia_capacity"))
                         {
                             var remainingCapacity = vesselCapacity.GetAttributeValue<decimal>("dia_capacity") - quantity;
@@ -35,7 +33,33 @@ namespace Disruptive_Advantage_Customization
                             vesselUpdate.Attributes["dia_remainingcapacity"] = remainingCapacity;
                             service.Update(vesselUpdate);
                         }*/
+                        #region Update Source Vessel Quantity and Composition
 
+                        var querySourceVessel = new QueryExpression("dia_jobsourcevessel");
+                        querySourceVessel.ColumnSet.AddColumns("dia_jobsourcevesselid", "dia_vessel", "dia_quantity");
+                        querySourceVessel.Criteria.AddCondition("dia_job", ConditionOperator.Equal, targetEntity.Id);
+
+                        EntityCollection resultsSourceVessel = service.RetrieveMultiple(querySourceVessel);
+
+                        foreach (var sourceVessel in resultsSourceVessel.Entities)
+                        {
+                            if(sourceVessel.GetAttributeValue<EntityReference>("dia_vessel") != null)
+                            {
+                                var vesselInformation = service.Retrieve(sourceVessel.GetAttributeValue<EntityReference>("dia_vessel").LogicalName, sourceVessel.GetAttributeValue<EntityReference>("dia_vessel").Id, new ColumnSet("dia_occupation", "dia_composition"));
+                                
+                                var sourceVesselUpdate = new Entity(vesselInformation.LogicalName);
+                                sourceVesselUpdate.Attributes["dia_occupation"] = vesselInformation.GetAttributeValue<decimal>("dia_occupation") - sourceVessel.GetAttributeValue<decimal>("dia_quantity");
+                                
+                                if(vesselInformation.GetAttributeValue<decimal>("dia_occupation") - sourceVessel.GetAttributeValue<decimal>("dia_quantity") == 0)
+                                {
+                                    sourceVesselUpdate.Attributes["dia_batch"] = null;
+                                    sourceVesselUpdate.Attributes["dia_composition"] = null;
+                                }
+                                service.Update(sourceVesselUpdate);
+                            }
+                        }
+                        #endregion
+                        #region Update Destination Vessel Quantity and Composition
                         var queryJobDestinationVessel = new QueryExpression("dia_jobdestinationvessel");
                         queryJobDestinationVessel.ColumnSet.AddColumns("dia_jobdestinationvesselid");
                         queryJobDestinationVessel.Criteria.AddCondition("dia_job", ConditionOperator.Equal, targetEntity.Id);
@@ -47,7 +71,52 @@ namespace Disruptive_Advantage_Customization
                             vesselUpdate.Attributes["statuscode"] = new OptionSetValue(914440001);
                             service.Update(vesselUpdate);
                         }
+                        #endregion
+                    }
+                    if(targetEntity.Contains("statuscode") && targetEntity.GetAttributeValue<OptionSetValue>("statuscode").Value == 914440000)
+                    {
+                        var jobType = service.Retrieve(targetEntity.LogicalName, targetEntity.Id, new ColumnSet("dia_jobtype"));
+                        if(jobType.Contains("dia_jobtype") && jobType.GetAttributeValue<OptionSetValue>("dia_jobtype") != null && jobType.GetAttributeValue<OptionSetValue>("dia_jobtype").Value == 914440001)
+                        {
+                            #region Update Source Vessel Quantity and Composition
+                            var querySourceVessel = new QueryExpression("dia_jobsourcevessel");
+                            querySourceVessel.ColumnSet.AddColumns("dia_jobsourcevesselid", "dia_vessel", "dia_quantity");
+                            querySourceVessel.Criteria.AddCondition("dia_job", ConditionOperator.Equal, targetEntity.Id);
 
+                            EntityCollection resultsSourceVessel = service.RetrieveMultiple(querySourceVessel);
+
+                            foreach (var sourceVessel in resultsSourceVessel.Entities)
+                            {
+                                if (sourceVessel.GetAttributeValue<EntityReference>("dia_vessel") != null)
+                                {
+                                    var vesselInformation = service.Retrieve(sourceVessel.GetAttributeValue<EntityReference>("dia_vessel").LogicalName, sourceVessel.GetAttributeValue<EntityReference>("dia_vessel").Id, new ColumnSet("dia_occupation", "dia_composition"));
+
+                                    var sourceVesselUpdate = new Entity(vesselInformation.LogicalName);
+                                    sourceVesselUpdate.Attributes["dia_occupation"] = vesselInformation.GetAttributeValue<decimal>("dia_occupation") - sourceVessel.GetAttributeValue<decimal>("dia_quantity");
+
+                                    if (vesselInformation.GetAttributeValue<decimal>("dia_occupation") - sourceVessel.GetAttributeValue<decimal>("dia_quantity") == 0)
+                                    {
+                                        sourceVesselUpdate.Attributes["dia_batch"] = null;
+                                        sourceVesselUpdate.Attributes["dia_composition"] = null;
+                                    }
+                                    service.Update(sourceVesselUpdate);
+                                }
+                            }
+                            #endregion
+                            #region Update Destination Vessel Quantity and Composition
+                            var queryJobDestinationVessel = new QueryExpression("dia_jobdestinationvessel");
+                            queryJobDestinationVessel.ColumnSet.AddColumns("dia_jobdestinationvesselid");
+                            queryJobDestinationVessel.Criteria.AddCondition("dia_job", ConditionOperator.Equal, targetEntity.Id);
+                            EntityCollection resultsQueryJobDestinationVessel = service.RetrieveMultiple(queryJobDestinationVessel);
+                            foreach (var vessel in resultsQueryJobDestinationVessel.Entities)
+                            {
+                                var vesselUpdate = new Entity(vessel.LogicalName);
+                                vesselUpdate.Id = vessel.Id;
+                                vesselUpdate.Attributes["statuscode"] = new OptionSetValue(914440001);
+                                service.Update(vesselUpdate);
+                            }
+                            #endregion
+                        }
                     }
                     #endregion
                 }
